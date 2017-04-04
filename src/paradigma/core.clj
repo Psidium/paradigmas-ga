32d9f648ca53c4aff4615b94a0c708b890a578a0
Psidium <psiidium@gmail.com>
2017-04-04 19:29:10 -0300

    add read and write from console

--- src/paradigma/core.clj
+++ src/paradigma/core.clj
@@ -4,4 +4,11 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [nome (do 
+               (println "Digite o nome de uma pessoa:") 
+               (read-line))
+        data (do 
+               (println "Digite a data de nascimento no formato ddMMaaaa") 
+               (read-line)) ]
+    (println (str " voce digitou " nome " e " data))))
+
