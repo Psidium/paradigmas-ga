5f1b5e6d50b5b6d32aaef901c5dfe13fbc222fa3
Psidium <psiidium@gmail.com>
2017-04-05 14:50:41 -0300

    add date calculation from java

--- src/paradigma/core.clj
+++ src/paradigma/core.clj
@@ -1,14 +1,25 @@
 (ns paradigma.core
-  (:gen-class))
+  (:gen-class)
+  (:import java.time.LocalDate)
+  (:import java.time.temporal.ChronoUnit))
+
+(defn pega-nome [] 
+  (println "Digite o nome de uma pessoa:") 
+  (read-line))
+(defn pega-nascimento []
+  (println "Digite a data de nascimento no formato ddMMaaaa") 
+  (read-line))
+
+(defn converte-data [dia]
+  (let [agora (LocalDate/now)
+        nascimento (LocalDate/of (read-string (subs dia 4)) (read-string (subs dia 2 4)) (read-string (subs dia 0 2))) ]
+    (.between (ChronoUnit/YEARS) nascimento agora)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (let [nome (do 
-               (println "Digite o nome de uma pessoa:") 
-               (read-line))
-        data (do 
-               (println "Digite a data de nascimento no formato ddMMaaaa") 
-               (read-line)) ]
-    (println (str " voce digitou " nome " e " data))))
+  (let [nome (pega-nome)
+        dia  (pega-nascimento)]
+    (println (str " voce digitou " nome " e " dia))
+    (println (str nome " tem " (converte-data dia) " anos") )
+    ))
 
